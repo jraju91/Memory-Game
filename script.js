4436ba99fc42d2df74cd1b33cbdb38a2ec730978
James <jamesraju91@gmail.com>
2022-03-08 18:37:35 -0500

    starting html/css/js

---
+++ script.js
@@ -0,0 +1,31 @@
+// whiteboarding
+/* 
+        I need 12 cards face down
+        when flipped those cards will reveal images/numbers
+
+        cards disappear once matched
+
+
+
+
+
+*/
+
+const cards = document.querySelectorAll('.cards')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cards.addEventListener()
